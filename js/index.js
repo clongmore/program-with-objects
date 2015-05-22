31ba778c5b3a2115cd06d906a894c5e844ff88bf
clongmore <carolynlongmore@gmail.com>
2015-05-22 15:53:39 -0400

    Committed by Udacity

---
+++ js/index.js
@@ -0,0 +1,28 @@
+
+Loading ..................
+
+Embed Details × Delete ⌘
+Last saved 2 minutes ago  
+Use Left Layout Use Top Layout Use Right Layout
+Embed This Pen
+Close
+Copy & Paste Code:
+WordPress Shortcode iframe HTML (recommended)
+Tab Bar PRO
+Default Tab
+Animations should...
+Border
+Border
+Tab Bar
+Tab Link
+Link & Logo
+Active Tab
+Active Link
+Custom CSS PRO
+learn more
+
+All Embedded Pens using this
+theme will be updated
+when saved.
+Theme:
+Live Preview
